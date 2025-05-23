4424df5ba05b196736e1b13b9498a63376fef540
Hacker01russ <dehup182022@yandex.com>
2025-05-23 12:54:00 +0300

    commitfour

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[]args){
         System.out.println("Проверка");
         System.out.println("Два");
+        System.out.println("Три");
     }
 }
