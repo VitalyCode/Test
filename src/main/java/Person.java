5fe831452f95661c06cac8f66d0226e44229526c
Hacker01russ <dehup182022@yandex.com>
2025-05-23 13:23:05 +0300

    change Commit

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,3 +1,9 @@
 public class Person {
-    public final int x = 1;
+    public int x = 1;
+    public Person(int x){
+        this.x = x;
+    }
+    public void method(){
+        System.out.println("Вывод: x"+x);
+    }
 }
